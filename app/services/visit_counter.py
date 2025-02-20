7bc0de97e2ab91981852404f5386f60a8bbbef09
Shreshthaaa <paliwalshreshtha@gmail.com>
2025-02-20 23:08:43 +0530

    feat: Implemented Counter using Redis

--- app/services/visit_counter.py
+++ app/services/visit_counter.py
@@ -5,8 +5,6 @@ from ..core.redis_manager import RedisManager
 from collections import defaultdict
 
 class VisitCounterService:
-    locks = defaultdict(asyncio.Lock)
-    visit_counter = defaultdict(int)
     def __init__(self):
         """Initialize the visit counter service with Redis manager"""
         self.redis_manager = RedisManager()
@@ -19,8 +17,7 @@ class VisitCounterService:
             page_id: Unique identifier for the page
         """
         # TODO: Implement visit count increment
-        async with VisitCounterService.locks[page_id]:
-            VisitCounterService.visit_counter[page_id] += 1
+        await self.redis_manager.increment(page_id, 1)
         pass
 
     async def get_visit_count(self, page_id: str) -> int:
@@ -34,6 +31,5 @@ class VisitCounterService:
             Current visit count
         """
         # TODO: Implement getting visit count
-        async with VisitCounterService.locks[page_id]:
-            return VisitCounterService.visit_counter[page_id]
-        return 0
+        count = await self.redis_manager.get(page_id)
+        return count
