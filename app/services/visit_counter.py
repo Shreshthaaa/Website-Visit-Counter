41176111d9f0746b383b1f8b25db74ac6e16519c
Shreshthaaa <paliwalshreshtha@gmail.com>
2025-02-21 23:20:25 +0530

    feat: Implemented Application Layer Caching

--- app/services/visit_counter.py
+++ app/services/visit_counter.py
@@ -1,10 +1,15 @@
 from typing import Dict, List, Any
 import asyncio
-from datetime import datetime
+from datetime import datetime, timedelta
 from ..core.redis_manager import RedisManager
 from collections import defaultdict
 
 class VisitCounterService:
+
+    visit_count_cache: Dict[str, Dict] = {}
+    cache_locks = defaultdict(asyncio.Lock)
+    timer = 30
+
     def __init__(self):
         """Initialize the visit counter service with Redis manager"""
         self.redis_manager = RedisManager()
@@ -18,7 +23,6 @@ class VisitCounterService:
         """
         # TODO: Implement visit count increment
         await self.redis_manager.increment(page_id, 1)
-        pass
 
     async def get_visit_count(self, page_id: str) -> int:
         """
@@ -31,5 +35,10 @@ class VisitCounterService:
             Current visit count
         """
         # TODO: Implement getting visit count
+        if page_id in VisitCounterService.visit_count_cache:
+            if (datetime.now() - VisitCounterService.visit_count_cache[page_id]["timestamp"]) < timedelta(seconds=VisitCounterService.timer):
+                return VisitCounterService.visit_count_cache[page_id]["count"]
+
         count = await self.redis_manager.get(page_id)
+        VisitCounterService.visit_count_cache[page_id] = {"count": count, "timestamp": datetime.now()}
         return count
