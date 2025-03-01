0b683d3232296b382df399343902876fe563fb14
Shreshthaaa <paliwalshreshtha@gmail.com>
2025-03-01 22:54:39 +0530

    feat: Implemented Batching Write Requestes to Redis

--- app/services/visit_counter.py
+++ app/services/visit_counter.py
@@ -3,17 +3,54 @@ import asyncio
 from datetime import datetime, timedelta
 from ..core.redis_manager import RedisManager
 from collections import defaultdict
-
+ 
 class VisitCounterService:
-
-    visit_count_cache: Dict[str, Dict] = {}
-    cache_locks = defaultdict(asyncio.Lock)
-    timer = 30
-
-    def __init__(self):
+ 
+    timer = 5
+    flush_interval = 30
+ 
+    def __init__(self, redis_manager: RedisManager):
         """Initialize the visit counter service with Redis manager"""
-        self.redis_manager = RedisManager()
-
+        self.redis_manager = redis_manager
+ 
+        # application layer cache for visit count
+        self.visit_count_cache: Dict[str, Dict] = {}
+        self.cache_locks = defaultdict(asyncio.Lock)
+ 
+        # write buffer for visit count
+        self.write_buffer = defaultdict(int)
+        self.buffer_locks = defaultdict(asyncio.Lock)
+ 
+        # start the buffer flush task
+        asyncio.create_task(self.flush_buffer())
+ 
+    async def flush_buffer(self) -> None:
+        """
+        Flush the write buffer to Redis
+        """
+        while True:
+            await asyncio.sleep(VisitCounterService.flush_interval)
+            for page_id in list(self.write_buffer.keys()):
+                await self.flush_buffer_key(page_id)
+ 
+    async def flush_buffer_key(self, page_id: str) -> None:
+        """
+        Flush the write buffer to Redis for a specific key
+        """
+        if page_id not in self.write_buffer:
+            return
+ 
+        async with self.buffer_locks[page_id]:
+            count = self.write_buffer[page_id]
+            if count > 0:
+                await self.redis_manager.increment(page_id, count)
+            self.write_buffer.pop(page_id)
+        self.buffer_locks.pop(page_id)
+ 
+    def _cache_validity_check(self, page_id: str) -> bool:
+        return (page_id in self.visit_count_cache and 
+        (datetime.now() - self.visit_count_cache[page_id]["timestamp"]) < timedelta(seconds=self.timer))
+ 
     async def increment_visit(self, page_id: str) -> None:
         """
         Increment visit count for a page
@@ -21,9 +58,9 @@ class VisitCounterService:
         Args:
             page_id: Unique identifier for the page
         """
-        # TODO: Implement visit count increment
-        await self.redis_manager.increment(page_id, 1)
-
+        async with self.buffer_locks[page_id]:
+            self.write_buffer[page_id] += 1
+ 
     async def get_visit_count(self, page_id: str) -> int:
         """
         Get current visit count for a page
@@ -34,11 +71,31 @@ class VisitCounterService:
         Returns:
             Current visit count
         """
-        # TODO: Implement getting visit count
-        if page_id in VisitCounterService.visit_count_cache:
-            if (datetime.now() - VisitCounterService.visit_count_cache[page_id]["timestamp"]) < timedelta(seconds=VisitCounterService.timer):
-                return VisitCounterService.visit_count_cache[page_id]["count"]
-
-        count = await self.redis_manager.get(page_id)
-        VisitCounterService.visit_count_cache[page_id] = {"count": count, "timestamp": datetime.now()}
-        return count
+ 
+        visit_count = 0
+ 
+        if self._cache_validity_check(page_id):
+            # using in-memory cache
+            async with self.cache_locks[page_id]:
+                visit_count = self.visit_count_cache[page_id]["count"]
+        
+        else:
+            # flushing the data to redis before fetching the data
+            await self.flush_buffer_key(page_id)
+ 
+            # using redis cache 
+            visit_count = await self.redis_manager.get(page_id)
+            if visit_count is None:
+                visit_count = 0
+ 
+            # update in-memory cache
+            async with self.cache_locks[page_id]:
+                self.visit_count_cache[page_id] = {
+                    "count": visit_count,
+                    "timestamp": datetime.now()
+                }
+        
+        async with self.buffer_locks[page_id]:
+            visit_count += self.write_buffer[page_id]
+ 
+        return visit_count 
